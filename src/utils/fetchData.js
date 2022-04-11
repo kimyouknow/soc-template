08880fcf2454d49f16bb456c53baf314786ae252
yunho <kimyouknow@naver.com>
2022-04-12 07:29:41 +0900

    Feat: dom조작 util 추가
    $: quertySelctor
    $$: quertySelctorAll
    createElement

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,3 +6,32 @@ export async function fetchData(url) {
     console.log(err);
   }
 }
+
+export const createElement = ({ tagName, className, id, attrs }) => {
+  const element = document.createElement(tagName);
+  if (className) {
+    if (Array.isArray(className)) element.className = className.join(' ');
+    else element.className = className;
+  }
+  if (id) {
+    element.id = id;
+  }
+  if (attrs) {
+    Object.entries(attrs).forEach(([key, value]) => {
+      element.setAttribute(key, value);
+    });
+  }
+  return element;
+};
+
+export function $(selector, scope = document) {
+  if (!selector) throw 'no selector';
+
+  return scope.querySelector(selector);
+}
+
+export function $$(selector, scope = document) {
+  if (!selector) throw 'no selector';
+
+  return Array.from(scope.querySelectorAll(selector));
+}
--- src/utils/manuplateDom.js
+++
@@ -1,4 +0,0 @@
-export function setInheritance({ parent, child }) {
-  child.prototype = Object.create(parent.prototype);
-  child.prototype.constructor = child;
-}
